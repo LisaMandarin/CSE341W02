0a41e818afcbf92fd3267cdaddfb046a77bb72ae
LisaMandarin <chu23001@byui.edu>
2024-11-13 05:34:14 +0800

    team project done

--- routes/temple.js
+++ routes/temple.js
@@ -3,7 +3,23 @@ const temples = require('../controllers/temple.js');
 
 routes.get('/', temples.findAll);
 routes.get('/:temple_id', temples.findOne);
+routes.get('')
 
+// #swagger.parameters['body'] = {
+//     in: 'body',
+//     required: true,
+//     schema: { $ref: '#/definitions/newTemple'}
+// }
 routes.post('/', temples.create);
+
+// #swagger.parameters['body'] = {
+//     in: 'body',
+//     required: true,
+//     schema: { $ref: '#/definitions/updateTemple'}
+// }
+routes.put('/:id', temples.update)
+
+routes.delete('/:id', temples.delete)
+routes.delete('/', temples.deleteAll)
 
 module.exports = routes;
--- swagger.js
+++ swagger.js
@@ -6,7 +6,20 @@ const doc = {
         description: "Temple API",
     },
     host: "localhost:8080",
-    schemes: "http",
+    schemes: ["http"],
+    definitions: {
+        newTemple: {
+            temple_id: 555,
+            name: "any",
+            location: "any",
+            dedicated: "2024-01-12",
+          },
+        updateTemple: {
+            name: "new",
+            location: "new",
+            dedicated: "2024-12-22",
+          },
+    }
 };
 
 const outputFile = "./swagger.json";
